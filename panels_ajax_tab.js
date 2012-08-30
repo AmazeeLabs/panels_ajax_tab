7e4624e9faf531b6063ce951069b500c0560ef10
Patrick Hayes <patrick.d.hayes@gmail.com>
2012-08-30 16:53:58 -0700

    Initial Commit

---
+++ panels_ajax_tab.js
@@ -0,0 +1,28 @@
+(function ($) {
+  /**
+   * JS related to the tabs in the Panels tabs.
+   */
+  Drupal.behaviors.panels_ajax_tabs = {
+    attach: function (context) {
+      $(function() {
+        $('.panels-ajax-tab-tab:not(.panels-ajax-tabs-processed)')
+          .click(function(){
+            var target_id      = $(this).data('target-id');
+            var panel_name     = $(this).data('panel-name');
+            var entity_context = $(this).data('entity-context');
+            $.ajax({
+              url: '/panels_ajax_tab/' + panel_name + '/' + entity_context,
+              datatype:'html'
+            }).done(function(data) {
+              $('#panels-ajax-tab-container-' + target_id).html(data);
+              drupal.attachBehaviors($('#panels-ajax-tab-container-' + target_id));
+            });
+            $(this).parent().siblings().removeClass('active');
+            $(this).parent().addClass('active');
+          })
+          .css('cursor','pointer')
+          .addClass('panels-ajax-tabs-processed');
+      });
+    }
+  };
+})(jQuery);

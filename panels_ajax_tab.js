eab7ccf24d991b9bc362a01e1f884a0c29b5053c
Patrick Hayes <patrick.d.hayes@gmail.com>
2012-09-06 14:31:20 -0700

    Better context handling

--- panels_ajax_tab.js
+++ panels_ajax_tab.js
@@ -5,7 +5,7 @@
   Drupal.behaviors.panels_ajax_tabs = {
     attach: function (context) {
       $(function() {
-        $('.panels-ajax-tab-tab:not(.panels-ajax-tabs-processed)')
+        $('.panels-ajax-tab-tab:not(.panels-ajax-tabs-processed)', context)
           .click(function(){
             var target_id      = $(this).data('target-id');
             var panel_name     = $(this).data('panel-name');
@@ -15,15 +15,16 @@
               datatype:'html'
             }).done(function(data) {
               $('#panels-ajax-tab-container-' + target_id).html(data);
-              drupal.attachBehaviors($('#panels-ajax-tab-container-' + target_id));
+              Drupal.attachBehaviors($('#panels-ajax-tab-container-' + target_id));
             });
             $(this).parent().siblings().removeClass('active');
             $(this).parent().addClass('active');
           })
           .css('cursor','pointer')
           .addClass('panels-ajax-tabs-processed');
+
         // Trigger a click event on the first tab to load it
-        $('.panels-ajax-tab-tab:not(.panels-ajax-tabs-first-loaded)').first()
+        $('.panels-ajax-tab-tab:not(.panels-ajax-tabs-first-loaded)', context).first()
           .trigger('click')
           .addClass('panels-ajax-tabs-first-loaded');
       });

77569c91123a34d74f302bcb6530956220b54338
Patrick Hayes <patrick.d.hayes@gmail.com>
2012-10-09 14:54:09 -0700

    Style cleanup - gracefully handling errors

--- js/panels_ajax_tab.js
+++ js/panels_ajax_tab.js
@@ -7,29 +7,33 @@
       $(function() {
         $('.panels-ajax-tab-tab:not(.panels-ajax-tabs-processed)', context)
             .click(function() {
-          var container = $(this).parents('.panels-ajax-tab:first');
-          if ($(container).data('loading') === true)
-            return true;
-          $(container).data('loading', true);
-
-          var target_id = $(this).data('target-id');
-          var panel_name = $(this).data('panel-name');
-          var entity_context = $(this).data('entity-context');
-          $.ajax({
-            url: Drupal.settings.basePath + 'panels_ajax_tab/' + panel_name + '/' + entity_context,
-            datatype: 'html',
-            cache: true,
-            beforeSend: function(xhr) {
-              $('#panels-ajax-tab-container-' + target_id).html('<img src="' + Drupal.settings.basePath + Drupal.settings.panel_ajax_tab.path + '/images/loading.gif"/>');
-            }
-          }).done(function(data) {
-            $('#panels-ajax-tab-container-' + target_id).html(data);
-            Drupal.attachBehaviors($('#panels-ajax-tab-container-' + target_id));
-            $(container).data('loading', false);
-          });
-          $(this).parent().siblings().removeClass('active');
-          $(this).parent().addClass('active');
-        })
+              var container = $(this).parents('.panels-ajax-tab:first');
+              if ($(container).data('loading') === true)
+                return true;
+              $(container).data('loading', true);
+    
+              var target_id = $(this).data('target-id');
+              var panel_name = $(this).data('panel-name');
+              var entity_context = $(this).data('entity-context');
+              $.ajax({
+                url: Drupal.settings.basePath + 'panels_ajax_tab/' + panel_name + '/' + entity_context,
+                datatype: 'html',
+                cache: true,
+                beforeSend: function(xhr) {
+                  $('#panels-ajax-tab-container-' + target_id).html('<img src="' + Drupal.settings.basePath + Drupal.settings.panel_ajax_tab.path + '/images/loading.gif"/>');
+                }, 
+                error: function(jqXHR, textStatus, errorThrown) {
+                  $('#panels-ajax-tab-container-' + target_id).html('Error: ' + errorThrown);
+                  $(container).data('loading', false);
+                }
+              }).done(function(data) {
+                $('#panels-ajax-tab-container-' + target_id).html(data);
+                Drupal.attachBehaviors($('#panels-ajax-tab-container-' + target_id));
+                $(container).data('loading', false);
+              });
+              $(this).parent().siblings().removeClass('active');
+              $(this).parent().addClass('active');
+            })
             .css('cursor', 'pointer')
             .addClass('panels-ajax-tabs-processed');
 

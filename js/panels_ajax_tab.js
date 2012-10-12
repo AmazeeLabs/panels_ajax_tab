75fbe964cdfb420ccc7fa28da36e629e2ac29d48
Patrick Hayes <patrick.d.hayes@gmail.com>
2012-10-12 11:35:04 -0700

    seperating out trigger into a jQuery plugin so it can be called idependantly. It can also take a callback.

--- js/panels_ajax_tab.js
+++ js/panels_ajax_tab.js
@@ -1,39 +1,11 @@
 (function($) {
-  /**
-   * JS related to the tabs in the Panels tabs.
-   */
   Drupal.behaviors.panels_ajax_tabs = {
     attach: function(context) {
       $(function() {
         $('.panels-ajax-tab-tab:not(.panels-ajax-tabs-processed)', context)
             .click(function(e) {
               e.preventDefault();
-              var container = $(this).parents('.panels-ajax-tab:first');
-              if ($(container).data('loading') === true)
-                return true;
-              $(container).data('loading', true);
-
-              var target_id = $(this).data('target-id');
-              var panel_name = $(this).data('panel-name');
-              var entity_context = $(this).data('entity-context');
-              $.ajax({
-                url: Drupal.settings.basePath + 'panels_ajax_tab/' + panel_name + '/' + entity_context,
-                datatype: 'html',
-                cache: true,
-                beforeSend: function(xhr) {
-                  $('#panels-ajax-tab-container-' + target_id).html('<img src="' + Drupal.settings.basePath + Drupal.settings.panel_ajax_tab.path + '/images/loading.gif"/>');
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                  $('#panels-ajax-tab-container-' + target_id).html('Error: ' + errorThrown);
-                  $(container).data('loading', false);
-                }
-              }).done(function(data) {
-                $('#panels-ajax-tab-container-' + target_id).html(data);
-                Drupal.attachBehaviors($('#panels-ajax-tab-container-' + target_id));
-                $(container).data('loading', false);
-              });
-              $(this).parent().siblings().removeClass('active');
-              $(this).parent().addClass('active');
+              $(this).panels_ajax_tabs_trigger();
             })
             .css('cursor', 'pointer')
             .addClass('panels-ajax-tabs-processed');
@@ -44,4 +16,49 @@
       });
     }
   };
+})(jQuery);
+
+
+/**
+ * Panels-ajax-tabs-trigger is a jquery plugin that can be triggered. 
+ * A callback, to be called after content has been loaded, can optionally be passed
+ */
+(function($){
+  $.fn.extend({ 
+    panels_ajax_tabs_trigger: function(callback) {
+      return this.each(function() {
+        var container = $(this).parents('.panels-ajax-tab:first');
+        if ($(container).data('loading') === true)
+          return true;
+        $(container).data('loading', true);
+
+        var target_id = $(this).data('target-id');
+        var panel_name = $(this).data('panel-name');
+        var entity_context = $(this).data('entity-context');
+        $.ajax({
+          url: Drupal.settings.basePath + 'panels_ajax_tab/' + panel_name + '/' + entity_context,
+          datatype: 'html',
+          cache: true,
+          beforeSend: function(xhr) {
+            $('#panels-ajax-tab-container-' + target_id).html('<img src="' + Drupal.settings.basePath + Drupal.settings.panel_ajax_tab.path + '/images/loading.gif"/>');
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            $('#panels-ajax-tab-container-' + target_id).html('Error: ' + errorThrown);
+            $(container).data('loading', false);
+          }
+        }).done(function(data) {
+          $('#panels-ajax-tab-container-' + target_id).html(data);
+          Drupal.attachBehaviors($('#panels-ajax-tab-container-' + target_id));
+          $(container).data('loading', false);
+          
+          // Trigger optional callback
+          if (callback) {
+            callback.call(this);
+          }
+        });
+        $(this).parent().siblings().removeClass('active');
+        $(this).parent().addClass('active');
+      });
+    }
+  });
 })(jQuery);
